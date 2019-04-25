a7699cf1272f3e417559e199e4db0f718addc43c
Vartul Tripathi <vartult@rediffmail.com>
2019-04-25 13:10:36 +0530

    Service

--- functions/index.js
+++ functions/index.js
@@ -4,13 +4,13 @@ const request = require('request-promise')
 
 exports.indexPostsToElastic = functions.database.ref('/posts/{post_id}')
 	.onWrite((change,context) => {
-		let postData = change.after.val();
+		let postData = change.after.val();  
 		let post_id = context.params.post_id;
 		
 		console.log('Indexing post:', postData);
 		
 		let elasticSearchConfig = functions.config().elasticsearch;
-		let elasticSearchUrl = elasticSearchConfig.url + 'posts/post/' + post_id;
+		let elasticSearchUrl = elasticSearchConfig.url + 'posts/' + post_id;
 		let elasticSearchMethod = postData ? 'POST' : 'DELETE';
 		
 		let elasticSearchRequest = {
